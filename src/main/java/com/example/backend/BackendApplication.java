5d768d4d8f3bd54bec8045450774d94cd176f160
TanPhat1706 <tanphaaat@gmail.com>
2025-12-29 11:30:42 +0700

    Fix timezone

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,8 +1,12 @@
 package com.example.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +16,10 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@PostConstruct
+    public void init() {
+        // Thiết lập múi giờ mặc định là Asia/Ho_Chi_Minh (GMT+7)
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        System.out.println("Spring boot application running in UTC+7 timezone :" + new java.util.Date());
+    }
 }
